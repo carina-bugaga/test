1a766c5d5ddad94214b8a144438d57d03ff9f940
carina-bugaga <98953836+carina-bugaga@users.noreply.github.com>
2023-05-17 15:13:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import "./styles.css";
 
 export function absentNumber(array) {
-  const arraySorted = array.sort();
-  console.log(arraySorted);
+  // Проверка пустого массива
+  if (array.length < 1) return 1;
+  // Проверка массива из одного элемента
+  if (array.length === 1) {
+    return array[0] === 1 ? 2 : array[0] - 1;
+  }
+  // Поиск максимального и минимального числа
+  let maxNumber = Math.max.apply(null, array);
+  let minNumber = Math.min.apply(null, array);
+  // Подсчет реальной и ожидаемой сумм
+  let actualSum = 0;
+  let expectedSum = 0;
+  array.forEach((element) => (actualSum += element));
+  for (let i = minNumber; i <= maxNumber; i++) {
+    expectedSum += i;
+  }
+  let sumDifference = expectedSum - actualSum;
+  // Поиск отсутствующего элемента
+  if (sumDifference > 0) return sumDifference;
+  else {
+    if (minNumber === 1) return maxNumber + 1;
+    else return minNumber - 1;
+  }
 }
+
